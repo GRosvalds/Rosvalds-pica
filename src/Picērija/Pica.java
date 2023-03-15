f3231995654f59912fb0deb987a19bd2d5228287
Rosvalds <Gabriels.Rosvalds@sk.lvt.lv>
2023-03-15 15:53:27 +0200

    Izveidots picas objekts

--- src/Picērija/Pica.java
+++ src/Picērija/Pica.java
@@ -1,5 +1,36 @@
 package Picçrija;
 
 public class Pica {
-
+	public String nosaukums;
+	public int izmers;
+	public boolean merce;
+	public boolean biezaPamatne;
+	public String sastavs;
+	
+	public Pica(String nosaukums, int izmers, boolean merce, boolean biezaPamatne, String sastavs) {
+		this.nosaukums = nosaukums;
+		this.izmers = izmers;
+		this.merce = merce;
+		this.biezaPamatne = biezaPamatne;
+		this.sastavs = sastavs;
+	}
+	public String getNosaukums(){
+		return nosaukums;
+	}
+	public int getIzmers(){
+		return izmers;
+	}
+	public boolean getMerce(){
+		return merce;
+	}
+	public boolean getBiezaPamatne(){
+		return biezaPamatne;
+	}
+	public String getSastavs(){
+		return sastavs;
+		
+	}
+	
+	
 }
+
--- src/Picērija/Picērija.java
+++ src/Picērija/Picērija.java
@@ -3,7 +3,7 @@ package Picçrija;
 public class Picçrija {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 

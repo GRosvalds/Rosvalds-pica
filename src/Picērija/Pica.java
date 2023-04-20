fcef3dc77a4ae53c43246ce3052d71512288be12
Rosvalds <Gabriels.Rosvalds@sk.lvt.lv>
2023-04-20 16:26:36 +0300

    izveidotas imainas faila

--- src/Picērija/Pica.java
+++ src/Picērija/Pica.java
@@ -1,4 +1,4 @@
-package PicÄ“rija;
+package Picçrija;
 
 public class Pica {
 	public Object izmers;
--- src/Picērija/Picērija.java
+++ src/Picērija/Picērija.java
@@ -1,4 +1,4 @@
-package PicÄ“rija;
+package Picçrija;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,7 +9,7 @@ import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-public class PicÄ“rija {
+public class Picçrija {
 
 	
 	public static int Spele(int nauda) {
@@ -21,23 +21,23 @@ public class PicÄ“rija {
 		ImageIcon toppings = new ImageIcon("toppings.jpg");
 		ImageIcon dough = new ImageIcon("dough.png");
 		ImageIcon oven = new ImageIcon("oven.png");
-		String[] jaunaIzvelne = {"SÄkt strÄdÄt", "Beigt strÄdÄt", "ApskatÄ«t picÄ“rijas informÄciju"};
+		String[] jaunaIzvelne = {"Sâkt strâdât", "Beigt strâdât", "Apskatît picçrijas informâciju"};
 		String izvele;
 		int Klienti = 0;
 		int apmierKlienti = 0;
 		int neapmierKlienti = 0;
 		do {
-			izvele= (String)JOptionPane.showInputDialog(null, "IzvÄ“lies darbÄ«bu \nJÅ«su nauda: "+nauda+" euro", "PaprikaPizza", JOptionPane.INFORMATION_MESSAGE, logo, jaunaIzvelne, jaunaIzvelne[0]);
+			izvele= (String)JOptionPane.showInputDialog(null, "Izvçlies darbîbu \nJûsu nauda: "+nauda+" euro", "PaprikaPizza", JOptionPane.INFORMATION_MESSAGE, logo, jaunaIzvelne, jaunaIzvelne[0]);
 			switch(izvele) {
-			case "SÄkt strÄdÄt":
+			case "Sâkt strâdât":
 				Random rand = new Random();
 			    Klienti+=1;
 				Object[] izmers = {20, 30, 50};
-			    String[] klients = {"PÄ“teris", "Anna", "Samanta", "Ralfs", "Maksims", "MÄris", "MatÄ«ss", "Keita", "Lidija", "JÄnis", "Maikls"};
-			   String[] pamatne = {"Bieza", "PlÄna", "Ar Sieru"};
-			   String[] merces = {"TomÄtu", "BÄrbekjÅ«", "Ä¶iploku", "SaldskÄbÄ", "Karija", "AsÄ"};
-			   String[] piedevas = {"Peperoni", "SÄ“nes", "Ananasi", "Bekons", "Vista", "MaltÄ gaÄ¼a"};
-			   String[] siers = {"Mocarellas", "ÄŒedaras", "Parasts", "KamambÄ“ras"};
+			    String[] klients = {"Pçteris", "Anna", "Samanta", "Ralfs", "Maksims", "Mâris", "Matîss", "Keita", "Lidija", "Jânis", "Maikls"};
+			   String[] pamatne = {"Bieza", "Plâna", "Ar Sieru"};
+			   String[] merces = {"Tomâtu", "Bârbekjû", "Íiploku", "Saldskâbâ", "Karija", "Asâ"};
+			   String[] piedevas = {"Peperoni", "Sçnes", "Ananasi", "Bekons", "Vista", "Maltâ gaïa"};
+			   String[] siers = {"Mocarellas", "Èedaras", "Parasts", "Kamambçras"};
 			   
 			   Object randIzmers = izmers[rand.nextInt(izmers.length)];
 			   String randKlients = klients[rand.nextInt(klients.length)];
@@ -49,21 +49,21 @@ public class PicÄ“rija {
 			   
 //			   int izmers, String merce, String pamatne, String piedevas, String siers
 			   Pica pica = new Pica(randIzmers, randMerce, randPamatne, randPiedevas, randSiers);
-			    String pasutijums = "Klients: "+randKlients+" \npasÅ«tija picu ar: \nPamatne: "+randPamatne+" \nIzmers: "+randIzmers+
-			   "cm \nMÄ“rce: "+randMerce+" \nPiedevas: "+randPiedevas+" \nSiers: "+randSiers;
+			    String pasutijums = "Klients: "+randKlients+" \npasûtija picu ar: \nPamatne: "+randPamatne+" \nIzmers: "+randIzmers+
+			   "cm \nMçrce: "+randMerce+" \nPiedevas: "+randPiedevas+" \nSiers: "+randSiers;
 			    
-			    JOptionPane.showMessageDialog(null, "JÅ«su pasÅ«tÄ«jums: \n"+pasutijums, "PasÅ«tÄ«jums", JOptionPane.INFORMATION_MESSAGE);
-			    int pIzmers = (int) JOptionPane.showInputDialog(null,"IzmÄ“rs: ", "Ceptuve", JOptionPane.INFORMATION_MESSAGE, null, izmers, izmers[0]);
+			    JOptionPane.showMessageDialog(null, "Jûsu pasûtîjums: \n"+pasutijums, "Pasûtîjums", JOptionPane.INFORMATION_MESSAGE);
+			    int pIzmers = (int) JOptionPane.showInputDialog(null,"Izmçrs: ", "Ceptuve", JOptionPane.INFORMATION_MESSAGE, null, izmers, izmers[0]);
 			    String pPamatne;
 			    pPamatne = (String)JOptionPane.showInputDialog(null, "Pamatne: ", "Ceptuve", JOptionPane.INFORMATION_MESSAGE,dough, pamatne, pamatne[0]);
 			    String pMerce = (String)JOptionPane.showInputDialog(null, "Merce: ", "Ceptuve", JOptionPane.INFORMATION_MESSAGE,souce, merces, merces[0]);
 			    String pSiers = (String)JOptionPane.showInputDialog(null, "Siers: ", "Ceptuve", JOptionPane.INFORMATION_MESSAGE,cheese, siers, siers[0]);
 			    String pPiedevas = (String)JOptionPane.showInputDialog(null, "Piedevas: ", "Ceptuve", JOptionPane.INFORMATION_MESSAGE,toppings, piedevas, piedevas[0]);
 			    int kludas = 0;
-			    JOptionPane.showMessageDialog(null, "Pica ielikta krÄsnÄ«!", "KrÄsns", JOptionPane.INFORMATION_MESSAGE, oven);
+			    JOptionPane.showMessageDialog(null, "Pica ielikta krâsnî!", "Krâsns", JOptionPane.INFORMATION_MESSAGE, oven);
 			    int seconds = 10; 
 		        for (int i = seconds; i >= 0; i--) {
-		           System.out.println("AtlikuÅ¡ais laiks: "+ i + " sekundes");
+		           System.out.println("Atlikuğais laiks: "+ i + " sekundes");
 		            try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -90,7 +90,7 @@ public class PicÄ“rija {
 			    }
 			    
 			    if(kludas>=2) {
-			    	JOptionPane.showMessageDialog(null, randKlients+" nesamaksÄja par picu, un neatstÄja dzeramnaudu, jo\npieÄ¼ÄvÄt pÄrÄk daudz kÄ¼Å«du picas pagatavoÅ¡anÄ!", "pasÅ«tÄ«jums", JOptionPane.INFORMATION_MESSAGE, dislike);
+			    	JOptionPane.showMessageDialog(null, randKlients+" nesamaksâja par picu, un neatstâja dzeramnaudu, jo\npieïâvât pârâk daudz kïûdu picas pagatavoğanâ!", "pasûtîjums", JOptionPane.INFORMATION_MESSAGE, dislike);
 			    	neapmierKlienti+=1;
 			    }else {
 			    if(pIzmers == (int)pica.getIzmers() && pPamatne.equalsIgnoreCase(pica.getPamatne()) && pMerce.equalsIgnoreCase(pica.getMerce()) && pSiers.equalsIgnoreCase(pica.getSiers()) && pPiedevas.equalsIgnoreCase(pica.getPiedevas())) {
@@ -98,56 +98,56 @@ public class PicÄ“rija {
 			    	nauda += 20;
 			    	int tips = rand.nextInt(10)+0;
 			    	nauda +=tips;
-			    	JOptionPane.showMessageDialog(null, randKlients+" samaksÄja par picu "+20+" euro, un atstÄja "+tips+" euro dzeramnaudu", "pasÅ«tÄ«jums", JOptionPane.INFORMATION_MESSAGE, like);
+			    	JOptionPane.showMessageDialog(null, randKlients+" samaksâja par picu "+20+" euro, un atstâja "+tips+" euro dzeramnaudu", "pasûtîjums", JOptionPane.INFORMATION_MESSAGE, like);
 			    }
 			    
 			    else if(pPamatne.equalsIgnoreCase(pica.getPamatne()) && pMerce.equalsIgnoreCase(pica.getMerce()) && pSiers.equalsIgnoreCase(pica.getSiers()) && pPiedevas.equalsIgnoreCase(pica.getPiedevas())){
 			    	nauda+=20;
 			    	apmierKlienti+=1;
-			    	JOptionPane.showMessageDialog(null, randKlients+" samaksÄja par picu "+20+" euro, un atstÄja "+0+" euro dzeramnaudu, jo\nuztaisijÄt picu ar nepareizu izmÄ“ru!", "pasÅ«tÄ«jums", JOptionPane.INFORMATION_MESSAGE, like);
+			    	JOptionPane.showMessageDialog(null, randKlients+" samaksâja par picu "+20+" euro, un atstâja "+0+" euro dzeramnaudu, jo\nuztaisijât picu ar nepareizu izmçru!", "pasûtîjums", JOptionPane.INFORMATION_MESSAGE, like);
 
 			    }
 			    else if(pIzmers == (int)pica.getIzmers() && pMerce.equalsIgnoreCase(pica.getMerce()) && pSiers.equalsIgnoreCase(pica.getSiers()) && pPiedevas.equalsIgnoreCase(pica.getPiedevas())){
 			    	nauda+=20;
 			    	int tips = rand.nextInt(7)+0;
 			    	apmierKlienti+=1;
-			    	JOptionPane.showMessageDialog(null, randKlients+" samaksÄja par picu "+20+" euro, un atstÄja "+tips+" euro dzeramnaudu.\nJÅ«s uztaisijÄt picu ar nepareizu pamatni, lÅ«dzu pievÄ“rsiet uzmanÄ«bu!", "pasÅ«tÄ«jums", JOptionPane.WARNING_MESSAGE, like);
+			    	JOptionPane.showMessageDialog(null, randKlients+" samaksâja par picu "+20+" euro, un atstâja "+tips+" euro dzeramnaudu.\nJûs uztaisijât picu ar nepareizu pamatni, lûdzu pievçrsiet uzmanîbu!", "pasûtîjums", JOptionPane.WARNING_MESSAGE, like);
 
 			    }
 			    else if(pIzmers == (int)pica.getIzmers() && pPamatne.equalsIgnoreCase(pica.getPamatne()) && pSiers.equalsIgnoreCase(pica.getSiers()) && pPiedevas.equalsIgnoreCase(pica.getPiedevas())){
 			    	nauda+=15;
 			    	neapmierKlienti+=1;
 			    	
-			    	JOptionPane.showMessageDialog(null, randKlients+" samaksÄja par picu "+15+" euro, un atstÄja "+0+" euro dzeramnaudu.\nJÅ«s uztaisijÄt picu ar nepareizu mÄ“rci, lÅ«dzu pievÄ“rsiet uzmanÄ«bu!", "pasÅ«tÄ«jums", JOptionPane.WARNING_MESSAGE, dislike);
+			    	JOptionPane.showMessageDialog(null, randKlients+" samaksâja par picu "+15+" euro, un atstâja "+0+" euro dzeramnaudu.\nJûs uztaisijât picu ar nepareizu mçrci, lûdzu pievçrsiet uzmanîbu!", "pasûtîjums", JOptionPane.WARNING_MESSAGE, dislike);
 
 			    }
 			    else if(pIzmers == (int)pica.getIzmers() && pPamatne.equalsIgnoreCase(pica.getPamatne()) &&pMerce.equalsIgnoreCase(pica.getMerce()) &&pPiedevas.equalsIgnoreCase(pica.getPiedevas())){
 			    	nauda+=10;
 			    	neapmierKlienti+=1;
 			    	
-			    	JOptionPane.showMessageDialog(null, randKlients+" samaksÄja par picu "+10+" euro, un atstÄja "+0+" euro dzeramnaudu.\nJÅ«s uztaisijÄt picu ar nepareizu sieru, lÅ«dzu pievÄ“rsiet uzmanÄ«bu!", "pasÅ«tÄ«jums", JOptionPane.WARNING_MESSAGE,dislike);
+			    	JOptionPane.showMessageDialog(null, randKlients+" samaksâja par picu "+10+" euro, un atstâja "+0+" euro dzeramnaudu.\nJûs uztaisijât picu ar nepareizu sieru, lûdzu pievçrsiet uzmanîbu!", "pasûtîjums", JOptionPane.WARNING_MESSAGE,dislike);
 
 			    }
 			    else if(pIzmers == (int)pica.getIzmers() && pPamatne.equalsIgnoreCase(pica.getPamatne()) && pSiers.equalsIgnoreCase(pica.getSiers()) && pMerce.equalsIgnoreCase(pica.getMerce())){
 			    	nauda+=5;
 			    	neapmierKlienti+=1;
 			    	
-			    	JOptionPane.showMessageDialog(null, randKlients+" samaksÄja par picu "+5+" euro, un atstÄja "+0+" euro dzeramnaudu.\nJÅ«s uztaisijÄt picu ar nepareizÄm piedevÄm, lÅ«dzu pievÄ“rsiet uzmanÄ«bu!", "pasÅ«tÄ«jums", JOptionPane.WARNING_MESSAGE, dislike);
+			    	JOptionPane.showMessageDialog(null, randKlients+" samaksâja par picu "+5+" euro, un atstâja "+0+" euro dzeramnaudu.\nJûs uztaisijât picu ar nepareizâm piedevâm, lûdzu pievçrsiet uzmanîbu!", "pasûtîjums", JOptionPane.WARNING_MESSAGE, dislike);
 
 			    }
 			    }
 			    break;
 			    
 				
-			case "ApskatÄ«t picÄ“rijas informÄciju":
-				JOptionPane.showMessageDialog(null, "Å odien jÅ«su picÄ“riju apmeklÄ“ja "+Klienti+" klients/i\n"+apmierKlienti+" no tiem bija apmierinÄts/i, bet "+neapmierKlienti+" neapmierinÄts/i");
+			case "Apskatît picçrijas informâciju":
+				JOptionPane.showMessageDialog(null, "Ğodien jûsu picçriju apmeklçja "+Klienti+" klients/i\n"+apmierKlienti+" no tiem bija apmierinâts/i, bet "+neapmierKlienti+" neapmierinâts/i");
 				break;
 				
-			case "Beigt strÄdÄt":
+			case "Beigt strâdât":
 				
 				break;
 			}
-		}while(!izvele.equalsIgnoreCase("Beigt strÄdÄt"));
+		}while(!izvele.equalsIgnoreCase("Beigt strâdât"));
 		return nauda;
 	    
 	
@@ -159,38 +159,39 @@ public class PicÄ“rija {
 	    int nauda = 0;
 	    String izvele;
 	    String[] izvelne = {"Sakt jaunu speli", "Ieladet speli", "Iziet"};
-	    String[] jaunaIzvelne = {"SÄkt strÄdÄt", "Beigt strÄdÄt", "ApskatÄ«t picÄ“rijas informÄciju"};
+	    String[] jaunaIzvelne = {"Sâkt strâdât", "Beigt strâdât", "Apskatît picçrijas informâciju"};
 	    do {
-	        izvele = (String) JOptionPane.showInputDialog(null, "IzvÄ“lies darbÄ«bu", "PaprikaPizza", JOptionPane.INFORMATION_MESSAGE, logo, izvelne, izvelne[0]);
+	        izvele = (String) JOptionPane.showInputDialog(null, "Izvçlies darbîbu", "PaprikaPizza", JOptionPane.INFORMATION_MESSAGE, logo, izvelne, izvelne[0]);
 	        switch(izvele) {
 	        case "Sakt jaunu speli":
-	            String profilaVards = JOptionPane.showInputDialog(null, "Ievadi savu profilu vÄrdu:", "RegistrÄcija");
+	            String profilaVards = JOptionPane.showInputDialog(null, "Ievadi savu profilu vârdu:");
+	            nauda = 0;
 	            if (!profilaVards.trim().isEmpty()) {
 	                try {
 	                    File profilaFails = new File(System.getProperty("user.home") + "\\Desktop\\" + profilaVards + ".txt");
 	                    if (profilaFails.createNewFile()) {
-	                        JOptionPane.showMessageDialog(null, "Profilu veiksmÄ«gi saglabÄts!");
+	                        JOptionPane.showMessageDialog(null, "Profilu veiksmîgi saglabâts!");
 	                        nauda = Spele(nauda);
 		                    FileWriter rakstits = new FileWriter(profilaFails);
-		                    rakstits.write("Profila vÄrds: " + profilaVards + "\nNopelnita nauda: " + nauda);
+		                    rakstits.write("Profila vârds: " + profilaVards + "\nNopelnita nauda: " + nauda);
 		                    rakstits.close();
 	                    } else {
-	                        JOptionPane.showMessageDialog(null, "Profilu nevarÄ“ja saglabÄt. MÄ“Ä£ini vÄ“lreiz.");
+	                        JOptionPane.showMessageDialog(null, "Profilu nevarçja saglabât. Mçìini vçlreiz.");
 	                    }
 	                    
 	                } catch (IOException e) {
 	                    e.printStackTrace();
 	                }
 	            } else {
-	                JOptionPane.showMessageDialog(null, "Profilu vÄrds nevar bÅ«t tukÅ¡s!");
+	                JOptionPane.showMessageDialog(null, "Profilu vârds nevar bût tukğs!");
 	            }
 	            break;
 	        case "Ieladet speli":
-	            String profilaVards2 = JOptionPane.showInputDialog(null, "Ievadi profilu vÄrdu, kuru vÄ“lies ielÄdÄ“t:", "Profils");
-	            String profilaCeÄ¼Å¡ = System.getProperty("user.home") + "\\Desktop\\" + profilaVards2 + ".txt";
-	            if (new File(profilaCeÄ¼Å¡).exists()) {
+	            String profilaVards2 = JOptionPane.showInputDialog(null, "Ievadi profilu vârdu, kuru vçlies ielâdçt:");
+	            String profilaCeïğ = System.getProperty("user.home") + "\\Desktop\\" + profilaVards2 + ".txt";
+	            if (new File(profilaCeïğ).exists()) {
 	                try {
-	                    String saturaString = new String(Files.readAllBytes(Paths.get(profilaCeÄ¼Å¡)));
+	                    String saturaString = new String(Files.readAllBytes(Paths.get(profilaCeïğ)));
 	                    String[] lines = saturaString.split("\n");
 	                    if (lines.length >= 2) {
 	                        String[] moneyLineParts = lines[1].split(":");
@@ -203,17 +204,17 @@ public class PicÄ“rija {
 	                            }
 	                        }
 	                    }
-	                    JOptionPane.showMessageDialog(null, "Profilam " + profilaVards2 + " ir sekojoÅ¡s saturs:\n\n" + saturaString);
+	                    JOptionPane.showMessageDialog(null, "Profilam " + profilaVards2 + " ir sekojoğs saturs:\n\n" + saturaString);
 	                    nauda =Spele(nauda);
 	                    System.out.println(nauda);
-	                    FileWriter rakstits = new FileWriter(profilaCeÄ¼Å¡, false);
-	                    rakstits.write("Profila vÄrds: " + profilaVards2 + "\nNopelnta nauda: " + nauda);
+	                    FileWriter rakstits = new FileWriter(profilaCeïğ, false);
+	                    rakstits.write("Profila vârds: " + profilaVards2 + "\nNopelnta nauda: " + nauda);
 	                    rakstits.close();
 	                } catch (IOException e) {
 	                    e.printStackTrace();
 	                }
 	            } else {
-	                JOptionPane.showMessageDialog(null, "NevarÄ“ja atrast profilu ar vÄrdu " + profilaVards2 + ". MÄ“Ä£ini vÄ“lreiz.");
+	                JOptionPane.showMessageDialog(null, "Nevarçja atrast profilu ar vârdu " + profilaVards2 + ". Mçìini vçlreiz.");
 	            }
 	        }
 	    } while (!izvele.equalsIgnoreCase("Iziet"));
